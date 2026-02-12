73d71ed662e6da6307591aa3eb5e78ed7087bde7
Hyago Oliveira Diniz <137846861+Hyago-Oliveira-Diniz@users.noreply.github.com>
2026-02-12 19:26:14 -0300

    feat: cria entidade User com estrutura inicial
    feat: implementa entidade User com campos básicos e identificador único

--- src/main/java/br/com/hyago/minierp/model/User.java
+++ src/main/java/br/com/hyago/minierp/model/User.java
@@ -1,4 +1,40 @@
 package br.com.hyago.minierp.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String password;
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
+
